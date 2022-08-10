5c0113bd0bb9adbf4afeb1866d570078f95fe404
SaltSith <SaltSith66@gmail.com>
2022-08-10 18:17:27 +0200

    Read string from serial and split into substrings

--- Arduino -- arduShell/arduShell.cpp
+++ Arduino -- arduShell/arduShell.cpp
@@ -2,6 +2,19 @@
 
 #include <Arduino.h>
 
+#define ARDU_SHELL_MAX_ARGS					5/// Total args for commands
+
+static void arduShell_debug(String args[], uint8_t argc);
+
+static void arduShell_debug(String args[], uint8_t argc)
+{
+	Serial.println("argc = " + String(argc));
+
+	for (uint8_t arg = 0; arg <= argc; arg++) {
+		Serial.println("[" + String(arg) + "] " + args[arg]);
+	}
+}
+
 void arduShell_init(const uint32_t baudrate)
 {
 	Serial.begin(baudrate);
@@ -11,5 +24,34 @@ void arduShell_init(const uint32_t baudrate)
 
 void arduShell_main_loop(void)
 {
+	String data = "";
 
+	String args[ARDU_SHELL_MAX_ARGS];
+	uint8_t argc = 0;
+
+	if (Serial.available()) {
+		data = Serial.readString();
+	}
+
+	if (data == "") {
+		return;
+	}
+
+	data.replace("\n", "");
+	data.replace("\r", "");
+
+	for (uint16_t i = 0; i < data.length(); i++) {
+		if (data[i] == ' ') {
+			argc++;
+			continue;
+		}
+		if (argc >= ARDU_SHELL_MAX_ARGS) {
+			Serial.println("To many arguments!");
+			return;
+		}
+		args[argc] += data[i];
+	}
+
+	arduShell_debug(args, argc);
 }
+
