205f1807da8c8ade99b93f5aa68825200ffb79ef
SaltSith <SaltSith66@gmail.com>
2022-08-10 17:34:54 +0200

    Add project files.

---
+++ Arduino -- arduShell/Arduino -- arduShell.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Arduino____arduShell.ino
+ Created:	8/10/2022 5:34:44 PM
+ Author:	marcin
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
